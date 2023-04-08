4db42587f682eb161d8acfe546fb15fc8262ee95
Hong Li Ye <herman.ye@veeva.com>
2023-04-08 19:15:26 +0800

    Go

---
+++ src/test/java/com/example/devopsgithub/DevopsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
