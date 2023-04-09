58616c04b5e6ec3a2b8d9715ac62280587e2992a
Hong Li Ye <herman.ye@veeva.com>
2023-04-09 22:45:29 +0800

    go

--- src/main/java/com/example/devopsgithub/controller/JsonReponse.java
+++ src/main/java/com/example/devopsgithub/controller/JsonReponse.java
@@ -9,6 +9,12 @@ public class JsonReponse {
 
     @GetMapping("/student")
     public Student myStudent() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
         Student student = new Student("Alex", "Male", 21);
         return student;
     }
